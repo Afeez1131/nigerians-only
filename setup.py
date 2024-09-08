115945b355a02929d7564aa374fe01a612b76cb4
Lawal Afeez <lawalafeez052@gmail.com>
2024-09-08 06:48:51 +0100

    excluded tests, setup for live

--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup()
 
-  #
-# setup(
-#     name="django_nigerian_only",
-#     long_description_content_type='text/x-rst',
-#     version=nigerian_only.__version__, # "1.1.9.4",
-#     install_requires=[
-#         'Django>=4.1',
-#         'geoip2>=4.8.0',
-#         # other dependencies
-#     ],
-#     packages=find_packages(exclude=["tests"]),
-#     exclude_package_data={"": ["**/migrations/*"]},
-# )
+
+"""
+packages=find_packages(exclude=["tests", "tests.*", "migrations/*"]),
+        include_package_data=True,  # Ensure MANIFEST.in is respected
+        exclude_package_data={
+            '': ['*.pyc', '*.pyo', '*.pyd', '**/migrations/*']  # Exclude .pyc, .pyo, .pyd files and migration files
+        },
+"""
